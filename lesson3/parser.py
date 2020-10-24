21f3d9f0048c58c88fc9c1a9925e2da58510c3fd
Tamara Nelyubina <garpia1989@gmail.com.com>
2020-10-24 15:50:24 +0300

    Lesson3. ParserGB

---
+++ lesson3/parser.py
@@ -0,0 +1,128 @@
+from urllib.parse import urlparse
+import dateparser
+import requests
+from bs4 import BeautifulSoup
+
+
+class GBParser:
+    _params = {
+        'commentable_id': 0
+    }
+
+    _headers = {
+        'User_agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0',
+    }
+
+    def __init__(self, start_url, comments_url):
+        self.start_url = start_url
+        self._url = urlparse(start_url)
+        self.comments_url = comments_url
+
+    def _get_soup(self, soup_url: str, param=None):
+        if param:
+            params = {
+                'page': param
+            }
+            response = requests.get(soup_url, params=params, headers=self._headers)
+        else:
+            response = requests.get(soup_url, headers=self._headers)
+        return BeautifulSoup(response.text, 'lxml')
+
+    @staticmethod
+    def _get_tags(tag_soup, block, block_class):
+        tag_soup = tag_soup.findChildren(block, attrs={'class': block_class})
+        tags_dict = {}
+        for tag in tag_soup:
+            tag_url = tag.attrs.get('href')
+            tag_name = getattr(tag, 'text')
+            tags_dict[tag_name] = tag_url
+        return tags_dict
+
+    def _get_comments(self, p_id):
+        params = {
+            'commentable_id': p_id
+        }
+        post_comments = []
+        comments_response = requests.get(self.comments_url, params=params, headers=self._headers)
+        comments = comments_response.json()
+        for comment in comments:
+            while comment:
+                comment = comment['comment']
+                comment_dict = {'writer': comment['user']['full_name'],
+                                'url': comment['user']['url'],
+                                'text': comment['body']}
+                post_comments.append(comment_dict)
+
+                try:
+                    comment = comment['children'][0]
+                except IndexError:
+                    comment = 0
+        return post_comments
+
+    def parse(self, url_to_parse=None):
+        data = []
+        if not url_to_parse:
+            url_to_parse = self.start_url
+        count = 1
+        post_attrs = {}
+        soup = self._get_soup(url_to_parse, count)
+        while soup:
+            content = soup.find('div', attrs={'class': 'post-items-wrapper'})
+            for child in content.children:
+                post_url = child.findChild('a')
+                post_url = f'{self._url.scheme}://{self._url.hostname}{post_url.attrs.get("href")}'
+                post_attrs = {'url': post_url}
+
+                post_soup = self._get_soup(post_url)
+                post_soup = post_soup.find('article', attrs={'class': 'blogpost__article-wrapper'})
+
+                post_date = post_soup.find('time').attrs.get('datetime')
+                post_attrs['post_date'] = dateparser.parse(post_date)
+
+                post_attrs['title'] = getattr(post_soup.find('h1', attrs={'class': 'blogpost-title'}), 'text')
+
+                post_attrs['description'] = getattr(post_soup.find('div',
+                                                                   attrs={'class': 'blogpost-description'}), 'text')
+
+                post_attrs['post_img'] = post_soup.find('img').attrs.get('src')
+
+                writer = post_soup.find('div', attrs={'class': 'col-md-5'})
+
+                post_attrs['writer_name'] = getattr(writer.find('div', attrs={'itemprop': 'author'}), 'text')
+                post_attrs['writer_url'] = f'{self._url.scheme}://{self._url.hostname}' \
+                                           f'{writer.find("a").attrs.get("href")}'
+
+                post_attrs['tags'] = self._get_tags(post_soup, 'a', 'small')
+
+                post_id = post_soup.find('comments').attrs.get('commentable-id')
+                comments_total = post_soup.find('comments').attrs.get('total-comments-count')
+                comments_total = int(comments_total)
+                if comments_total == 0:
+                    post_attrs['comments'] = None
+                else:
+                    post_attrs['comments'] = self._get_comments(post_id)
+            data.append(post_attrs)
+            count += 1
+            print(count)
+            print(data)
+            soup = self._get_soup(url_to_parse, count)
+            if not soup.find('div', attrs={'class': 'post-items-wrapper'}):
+                soup = 0
+
+        return data, count
+
+    def save_to_sqlite(self, ):
+        pass
+
+
+if __name__ == '__main__':
+    url = 'https://geekbrains.ru/posts?page=1'
+    comment_url = 'https://geekbrains.ru/api/v2/comments?commentable_type=Post&commentable_id=&order=desc'
+    parser = GBParser(url, comment_url)
+    data_list, i = parser.parse()
+    print(i)
+    print(data_list)
+    # while i:
+    #     print('text')
+    # response = requests.get(url)
+    # print(BeautifulSoup(response.text, 'lxml'))
